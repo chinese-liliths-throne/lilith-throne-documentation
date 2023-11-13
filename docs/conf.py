b20bcb4cd88d037d93205b0f8e17040e6fcd52fb
CK_Rainbow <nanashichi@proton.me>
2023-11-13 12:54:33 +0800

    fix: error in extlinks

--- docs/conf.py
+++ docs/conf.py
@@ -83,15 +83,15 @@ html_baseurl = "https://bicobus.github.io/lilith-throne-documentation/"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 extlinks = {
     'wiki': (
         'https://www.lilithsthrone.com/wiki/doku.php?id=modding_wiki:modding:%s',
-        ''
+        '%s'
     ),
     'ltgithub': (
         'https://github.com/Innoxia/liliths-throne-public/tree/dev/%s',
-        ''
+        '%s'
     )
 }
